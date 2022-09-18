4e365ec1be8e9d6e95239e1caa77ce5c4c965167
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-09-18 15:38:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 2
+
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// int min = b;
+// if (a > b)
+// {
+//    max = a;
+//    min = b;
+// }
+// else
+// {
+//    max = b;
+//    min = a;
+// }
+// Console.Write("Максимальное: " + max + ", а минимальное: " + min);
+
+
+// Задача 4
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > b && a > c) max = a;
+// if (b > a && b > c) max = b;
+// if (c > a && c > b) max = c;
+// Console.Write("Максимальное: " + max);
+
+
+// Задача 6
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0) Console.Write("Да");
+// else Console.Write("Нет");
+
+
+// Задача 8
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i < a - 2; i++)
+// {
+//    if (i % 2 == 0)
+//    {
+//       Console.Write(i + ", ");
+//    }
+// }
+// for (int i = a - 2; i < a; i++)
+// {
+//    if (i % 2 == 0)
+//    {
+//       Console.Write(i);
+//    }
+// } // эта ересь, чтобы вывести в конце без запятой
